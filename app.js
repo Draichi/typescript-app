6a6e3db7e576ecdd4b6c66ffde8a2921a6523d45
draichi <lucasdraichi@gmail.com>
2020-06-11 20:44:41 -0300

    :zap:

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log('lucassss !!!');
+var add = function (n1, n2) { return n1 + n2; };
+var number1 = 1;
+var n2 = 2;
+console.log(add(number1, n2));
--- app.ts
+++ app.ts
@@ -1,1 +1,6 @@
-console.log('lucassss !!!')
+const add = (n1: number, n2: number) => n1 + n2
+
+const number1 = 1
+const n2 = 2
+
+console.log(add(number1, n2))
